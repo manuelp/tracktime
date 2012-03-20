907ec240f73a947f7dbe384a0110912539d2bf27
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-03-20 20:51:09 +0100

    First thing

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject tracktime "0.0.1-SNAPSHOT"
   :description "A simple CLI time tracker"
-  :url "https://bitbucket.org/manuelp/tracktime"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [joda-time/joda-time "2.0"]])
--- src/tracktime/core.clj
+++ src/tracktime/core.clj
@@ -1,4 +1,9 @@
-(ns tracktime.core)
+(ns tracktime.core
+  (:import [org.joda.time DateTime]
+           [java.text SimpleDateFormat])))
+
+(defn format-date-time [date]
+  (. (new SimpleDateFormat "dd/MM/yyyy") format (. date toDate)))
 
 (defn -main
   "I don't do a whole lot."
