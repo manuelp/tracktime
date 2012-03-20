6ccbdb88b5aaecc5d92e39fc73653c1f54bee733
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-03-20 22:29:34 +0100

    Can write tasks to CSV

--- src/tracktime/core.clj
+++ src/tracktime/core.clj
@@ -1,9 +1,7 @@
 (ns tracktime.core
-  (:import [org.joda.time DateTime Period]
+  (require [clojure.string :as s])
+  (import [org.joda.time DateTime Period]
            [java.text SimpleDateFormat]))
-
-(defn format-date-time [date]
-  (. (new SimpleDateFormat "dd/MM/yyyy") format (. date toDate)))
 
 (def validate-tasks (partial every? :desc))
 
@@ -25,6 +23,29 @@
                     (terminate %)
                     %) tasks))]
     (swap! tasks terminate-open-task)))
+
+(defn format-csv [task]
+  (letfn [(format-period [period]
+            (str (.getHours period) "h " (.getMinutes period) "m"))
+          (format-date-time [date]
+            (. (new SimpleDateFormat "dd/MM/yyyy HH:mm") format (. date toDate)))]
+    (format "\"%s\",\"%s\",\"%s\",\"%s\""
+            (format-date-time (:start task))
+            (:desc task)
+            (format-date-time (:end task))
+            (format-period (:period task)))))
+
+(defn write-csv [filename]
+  (letfn [(format-csv-tasks []
+            (s/join \newline (map format-csv @tasks)))]
+    (spit filename (format-csv-tasks))))
+
+;; TODO
+(defn read-tasks [filename]
+  (let [data (slurp filename)]
+    (if (not (empty? data))
+      (read-string data)
+      [])))
 
 (defn -main
   "I don't do a whole lot."
