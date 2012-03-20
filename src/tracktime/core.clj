3d04bf0230d2f7e2672e7cab73c8a4b4b2951a58
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-03-20 22:06:28 +0100

    Start and terminate single tasks

--- src/tracktime/core.clj
+++ src/tracktime/core.clj
@@ -1,9 +1,28 @@
 (ns tracktime.core
-  (:import [org.joda.time DateTime]
-           [java.text SimpleDateFormat])))
+  (:import [org.joda.time DateTime Period]
+           [java.text SimpleDateFormat]))
 
 (defn format-date-time [date]
   (. (new SimpleDateFormat "dd/MM/yyyy") format (. date toDate)))
+
+(def validate-tasks (partial every? :desc))
+
+(def tasks (atom [] :validator validate-tasks))
+
+(defn start-task [description]
+  (swap! tasks conj {:desc description :start (new DateTime)}))
+
+(defn terminate [task]
+  (let [end (new DateTime)
+        period (new Period (:start task) end)]
+    (assoc task :end end :period period)))
+
+(defn end-task []
+  (letfn [(terminate-open-task [tasks]
+            (map #(if (not (:end %))
+                    (terminate %)
+                    %) tasks))]
+    (swap! tasks terminate-open-task)))
 
 (defn -main
   "I don't do a whole lot."
