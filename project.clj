67dc30ba52032edeb5618438e80237e3f056a284
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-03-26 22:33:26 +0200

    Bump version number

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject tracktime "0.0.1-SNAPSHOT"
-  :description "A simple CLI time tracker"
+(defproject tracktime "0.0.1"
+  :description "A simple CLI time tracker."
   :dev-dependencies [[lein-marginalia "0.7.0"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [joda-time/joda-time "2.0"]])
