7aa34ad152c465ff7aa2ace777a20b3698a836f9
map <manuel.paccagnella@gmail.com>
2012-04-15 15:38:52 +0200

    Table for tasks

--- src/tracktime/gui.clj
+++ src/tracktime/gui.clj
@@ -1,5 +1,13 @@
 (ns tracktime.gui
-  (:use [seesaw core mig]))
+  (:use [seesaw core mig]
+        tracktime.core))
+
+(def today-table (table :model [
+                                :columns [{:key :start :text "Start"}
+                                          {:key :desc :text "Description"}
+                                          {:key :end :text "End"}
+                                          {:key :period :text "Duration"}]
+                                :rows @tasks]))
 
 (defn -main [& args]
   (invoke-later
@@ -8,7 +16,7 @@
                :content (mig-panel
                           :constraints ["", ""]
                           :items [[(button :text "Aggregated") "wrap"]
-                                  ["TODO: list of today's tasks" "span 6,growx,wrap"]
+                                  [(scrollable today-table) "span 6,grow,shrink 0,wrap"]
                                   [:separator "growx,span 6,wrap"]
                                   [(text :columns 30) "span 4,growx"] 
                                   [(button :text "Start")]
