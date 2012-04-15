ad5a208e499865644a439c84c4c63f7e0e94a4fe
map <manuel.paccagnella@gmail.com>
2012-04-15 20:08:25 +0200

    Aggregated tasks dialog with table

--- src/tracktime/gui.clj
+++ src/tracktime/gui.clj
@@ -2,6 +2,8 @@
   (:use [seesaw core mig table])
   (:require [tracktime.core :as core]))
 
+;; Let's define some components.
+
 (defn today-table-model []
   [:columns [{:key :start :text "Start"}
              {:key :desc :text "Description"}
@@ -11,16 +13,34 @@
 
 (def today-table (table :model (today-table-model)))
 
-(defn update-table [table]
+(defn update-table [table model]
   (-> table
       clear!
-      (config! :model (today-table-model))))
+      (config! :model model)))
 
 (def current-task-text (text :columns 30))
 
 (def stop-button (button :text "Stop" :enabled? false))
 
 (def start-button (button :text "Start"))
+
+(def aggregated-button (button :text "Aggregated"))
+
+(defn aggregated-table-model []
+  [:columns [{:key :desc :text "Description"}
+             {:key :minutes :text "Minutes"}]
+   :rows (core/aggregate-today)])
+
+(def aggregated-table (table :model (aggregated-table-model)))
+
+(def aggregated-dialog (dialog :modal? true
+                               :title "Today's aggregated tasks"
+                               :minimum-size [400 :by 200]
+                               :content (mig-panel
+                                         :constraints ["", ""]
+                                         :items [[(scrollable aggregated-table) "span 6 6,wrap"]])))
+
+;; Here comes the handlers...
 
 (defn start-task [e]
   (let [desc (value current-task-text)]
@@ -38,7 +58,12 @@
     (config! current-task-text :enabled? true)
     (config! start-button :enabled? true)
     (config! stop-button :enabled? false)
-    (update-table today-table)))
+    (update-table today-table (today-table-model))))
+
+(defn open-aggregated [e]
+  (show! aggregated-dialog))
+
+;; ...and the listen bindings.
 
 (listen start-button
         :mouse-clicked start-task)
@@ -46,13 +71,16 @@
 (listen stop-button
         :mouse-clicked end-task)
 
+(listen aggregated-button
+        :mouse-clicked open-aggregated)
+
 (defn -main [& args]
   (invoke-later
     (-> (frame :title "TrackTime"
                :resizable? false
                :content (mig-panel
                           :constraints ["", ""]
-                          :items [[(button :text "Aggregated") "wrap"]
+                          :items [[aggregated-button "wrap"]
                                   [(scrollable today-table) "span 6,grow,shrink 0,wrap"]
                                   [:separator "growx,span 6,wrap"]
                                   [current-task-text "span 4,growx"] 
