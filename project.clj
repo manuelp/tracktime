6cb58cb7f085b402169afff5347c6886b13faf72
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-04-19 23:01:54 +0200

    Painlessly updated to clojure 1.4 :)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tracktime "0.1.3"
+(defproject tracktime "0.1.4-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "A simple time tracker."
   :url "https://bitbucket.org/manuelp/tracktime"
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repl-retry-limit 30000
   :plugins [[lein-marginalia "0.7.0"]]
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [joda-time/joda-time "2.1"]
                  [seesaw "1.4.0"]]
   :main tracktime.gui)
